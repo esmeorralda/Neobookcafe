4895d65f56fa3f2762eecf31784ef8da463b8d92
esmeorralda <gsun51234@gmail.com>
2025-07-21 11:40:37 +1000

    right side bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs"
 Rails.start()
+
+document.addEventListener("turbo:load", function () {
+  const toggleBtn = document.getElementById("toggle-right-sidebar");
+  const sidebar = document.getElementById("right-sidebar");
+
+  if (toggleBtn && sidebar) {
+    toggleBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("hidden");
+    });
+  }
+});
+
